044ab8dc337abbbbcbba109b155a649252ae4583
Airxhi <ally@hvitt.co.uk>
2017-11-11 23:49:29 +0000

    almost almost working

--- csv_test.py
+++ csv_test.py
@@ -11,7 +11,7 @@ def main():
     orders.pop(0)
     for order in orders:
         order = list(map(int, order))
-        print (order)
+        #print (order)
 
 
     with open('product.csv') as csvDataFile:
@@ -38,6 +38,7 @@ class Product:
         self.length = length
         self.weight = weight
         self.category = catagory
+        #print (width, height, length)
         self.volume = height * width * length
 
 
@@ -69,6 +70,7 @@ class Product:
 
 
 class Box:
+    id = 0
     products = []
     cur_weight = 0
     weight = 16.0
@@ -79,6 +81,9 @@ class Box:
     cur_volume = 0
     max_zval = 0
     max_xval = 0
+
+    def __init__(self, id):
+        self.id = id
 
     def get_max_zval(self):
         return self.max_zval
@@ -120,36 +125,59 @@ def solve(order):
     products = []
     for prod in order:
         id = prod.get("SKU_ID")
-        width = prod.get("EACH_WIDTH")
-        height = prod.get("EACH.HEIGHT")
-        length = prod.get("EACH.LENGTH")
-        weight = prod.get("EACH_WEIGHT")
+        #width = prod.get("EACH_WIDTH")
+        width = prod['EACH_WIDTH']
+        #height = prod.get("EACH.HEIGHT")
+        length = prod["EACH_LENGTH"]
+        height = prod["EACH_HEIGHT"]
+        #weight = prod.get("EACH_WEIGHT")
+        weight = prod["EACH_WEIGHT"]
         category = prod.get("SEGREGATION_CATEGORY")
-        product = Product(id, width, length, height, category, weight)
+        product = Product(id, float(width), float(length), float(height), category, weight)
         product.normalize()
         products.append(product)
 
-    cat12 = filter(lambda x : x.get_category()==1 or x.get_category()==2, products)
-    cat3 = filter(lambda x : x.get_category()==3, products)
-    cat4 = filter(lambda x: x.get_category() == 4, products)
-    cat5 = filter(lambda x: x.get_category() == 5, products)
-    cat6 = filter(lambda x: x.get_category() == 6, products)
-    cat7 = filter(lambda x: x.get_category() == 7, products)
-    cat8 = filter(lambda x: x.get_category() == 8, products)
+    print (products)
 
+    cat12 = list(filter(lambda x: x.get_category()==1 or x.get_category()==2, products))
+    cat3 = list(filter(lambda x: x.get_category()==3, products))
+    cat4 = list(filter(lambda x: x.get_category() == 4, products))
+    cat5 = list(filter(lambda x: x.get_category() == 5, products))
+    cat6 = list(filter(lambda x: x.get_category() == 6, products))
+    cat7 = list(filter(lambda x: x.get_category() == 7, products))
+    cat8 = list(filter(lambda x: x.get_category() == 8, products))
+
+
+    #print (cat12)
     boxes = []
-    boxes.append(boxify(cat12))
-    boxes.append(boxify(cat3))
-    boxes.append(boxify(cat4))
-    boxes.append(boxify(cat5))
-    boxes.append(boxify(cat6))
-    boxes.append(boxify(cat7))
-    boxes.append(boxify(cat8))
+    box_id = 0
+    cat12_boxes, box_id = boxify(cat12,box_id)
+    cat3_boxes, box_id = boxify(cat3, box_id)
+    cat4_boxes, box_id = boxify(cat4, box_id)
+    cat5_boxes, box_id = boxify(cat5, box_id)
+    cat6_boxes, box_id = boxify(cat6, box_id)
+    cat7_boxes, box_id = boxify(cat7, box_id)
+    cat8_boxes, box_id = boxify(cat8, box_id)
+    boxes.append(cat12_boxes)
+    boxes.append(cat3_boxes)
+    boxes.append(cat4_boxes)
+    boxes.append(cat5_boxes)
+    boxes.append(cat6_boxes)
+    boxes.append(cat7_boxes)
+    boxes.append(cat8_boxes)
 
     result = []
     for boxs in boxes:
+        print (boxs)
         result += boxs
-    return boxs
+
+    final_result = []
+    for box in result:
+        for product in box.products:
+            final_result.append([box.id, product.id])
+
+    return final_result
+
 
 def category_sort(category):
     for i in range(1, len(category)):
@@ -173,9 +201,10 @@ def place_in_box(box, products):
 
 
 
-def boxify(category):
+def boxify(category,id):
     boxes = []
-    box = Box()
+    box = Box(id)
+    id+=1
     category = category_sort(category)
     while len(category) > 0:
         # find next best prod to add to boxy boi
@@ -188,7 +217,7 @@ def boxify(category):
             boxes.append(box)
             box = Box()
 
-    return boxes
+    return boxes, id
 
 def read_orders():
     with open('orderBoi.csv') as f:
@@ -196,41 +225,49 @@ def read_orders():
         orders = []
         for line in enumerate(r):
             orders.append(line)
-        f.close()
         return orders
 
+
 def read_my_lines(csv_reader, lines_list):
-    print (lines_list)
+    #print (lines_list)
     f.seek(0)
     for line_number, row in enumerate(csv_reader):
         for index in range (0, len(lines_list)):
             if (line_number == lines_list[index]):
                 yield line_number, row
 
+def read_my_lines_0(csv_reader, lines_list):
+    #print (lines_list)
+    f.seek(0)
+    for line_number, row in enumerate(csv_reader):
+        for index in range (0, len(lines_list)):
+            if (line_number == lines_list[index]-1):
+                yield line_number, row
 
 
 if __name__ == '__main__':
+    # Read in all the orders
     orders = read_orders()
-    #print orders
+    # Keep track of the total number of orders
     no_orders = int(orders[len(orders)-1][1]['ORDER_ID'])
-    #order_lists = [[]]*no_orders
+    # This will be a list of lists
+    # Each element is a list of the product IDs of the indexed order (+1)
     order_lists = []
     for i in range (0, no_orders):
         order_lists.append([])
     for i in range (0, len(orders)):
         index = int(orders[i][1]['ORDER_ID']) - 1
         order_lists[index].append(int(orders[i][1]['SKU_ID']))
-    #print order_lists
+
+    # Now we want to return a dictionary for every product in a particular order
     with open('product.csv') as f:
         r = csv.DictReader(f)
-        #print order_lists
-
         for i in range (0, len(order_lists)):
+            current_order_list = []
             for line_number, line in read_my_lines(r, order_lists[i]):
-                print (line_number, line)
-                #pass
-            #f.seek(0)
-            print ("END OF ORDER")
+                    #print (line_number, line)
+                current_order_list.append(line)
+            print (solve(current_order_list))
         f.close()
 
 #main()
